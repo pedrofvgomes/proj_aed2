bb3769998366022b23ff5a7af1c086cb1e7bb964
Pedro Gomes <up202108825@up.pt>
2023-01-04 14:46:07 +0000

    classe graph comentada, para evitar duvidas posteriores

--- graph.h
+++ graph.h
@@ -9,22 +9,29 @@ using namespace std;
 
 class Graph{
     struct Edge{
+        //aeroporto de destino
         string target;
+        //weight neste caso é distância, é automaticamente calculada de cada vez que adicionamos um adjacente
         float distance;
     };
 
     struct Node{
+        //todos os destinos possiveis
         list<Edge> adj;
+        //possivelmente nao vamos usar
         bool visited;
     };
 
+    //size
     int n;
+    //default true, porque os voos podem nao ter volta
     bool hasDir;
+    //string é o código do aeroporto, Node é o node correspondente
     map<string, Node> nodes;
 
 public:
-    Graph(int nodes, bool dir = false);
-    void addEdge(int src, int dest, int weight = 1);
+    Graph(int nodes, bool dir = true);
+    void addEdge(string src, string dest, float weight = 1.0);
 };
 
 #endif
