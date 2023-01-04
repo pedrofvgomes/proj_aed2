b56f2c88223ecf5ffe3502e2480c030c53368f5e
Pedro Gomes <up202108825@up.pt>
2023-01-04 01:53:05 +0000

    modificacoes a classe main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <map>
+#include "airport.cpp"
+#include "airline.cpp"
+#include "flight.cpp"
+#include "setup.cpp"
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    map<string, Airline> airlines = setupAirlines();
+    for(auto i:airlines){
+        cout<<i.first;
+        cout<< " ";
+        cout<<i.second.getCode();
+        cout<<i.second.getName();
+        cout<<i.second.getCallsign();
+        cout<<i.second.getCountry()<<endl;
+    }
     return 0;
 }
