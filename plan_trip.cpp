be6bbebde453d420d29f02bf4c978305d89bd2b4
Pedro Gomes <up202108825@up.pt>
2023-01-05 19:14:57 +0000

    funcao coordinateSearch, que devolve uma lista com os aeroportos num raio de x km a partir de uma coordenada

--- plan_trip.cpp
+++ plan_trip.cpp
@@ -2,12 +2,21 @@
 #include <string>
 #include "airport.h"
 #include <map>
+#include "graph.cpp"
 using namespace std;
 
 list<Airport> citySearch(const string& city, map<string,Airport> airports){
     list<Airport> a;
     for(auto i:airports)
         if(i.second.getCity()==city)
+            a.push_back(i.second);
+    return a;
+}
+
+list<Airport> coordinateSearch(const float& lat, const float& lon, const float& radius, map<string,Airport> airports){
+    list<Airport> a;
+    for(auto i:airports)
+        if(d(lat, i.second.getLatitude(), lon, i.second.getLongitude()) <= radius)
             a.push_back(i.second);
     return a;
 }
