ac82ce808deaff0a3fa543f5b85a7fb9926fbe35
Pedro Gomes <up202108825@up.pt>
2023-01-05 19:35:49 +0000

    funcao airlineRestriction, que devolve um vetor com os voos apenas de um determinado conjunto de companhias

--- plan_trip.cpp
+++ plan_trip.cpp
@@ -5,6 +5,8 @@
 #include "graph.cpp"
 using namespace std;
 
+//lista de aeroportos numa cidade
+
 list<Airport> citySearch(const string& city, map<string,Airport> airports){
     list<Airport> a;
     for(auto i:airports)
@@ -13,10 +15,25 @@ list<Airport> citySearch(const string& city, map<string,Airport> airports){
     return a;
 }
 
+//lista de aeroportos dentro de um raio x, a partir de uma coordenada fixa
+
 list<Airport> coordinateSearch(const float& lat, const float& lon, const float& radius, map<string,Airport> airports){
     list<Airport> a;
     for(auto i:airports)
         if(d(lat, i.second.getLatitude(), lon, i.second.getLongitude()) <= radius)
             a.push_back(i.second);
     return a;
+}
+
+//criar um grafo com restricao de companhias
+//----------------------------------------------------------------------------------
+//--------- vetor de companhias pode ser vector<Airline> ou vector<string> ---------
+//----------------------------------------------------------------------------------
+
+vector<Flight> airlineRestriction(vector<Flight> flights, vector<string> airlines){
+    vector<Flight> newflights;
+    for(auto i: flights)
+        for(auto a : airlines)
+            if(i.getAirline().getCode()==a) newflights.push_back(i);
+    return newflights;
 }
